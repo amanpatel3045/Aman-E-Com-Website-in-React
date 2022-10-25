8c2f512e03b54fd08736c5b9fc568639bd833cc8
amanpatel3045 <107243321+amanpatel3045@users.noreply.github.com>
2022-10-25 23:55:47 +0530

    NAV

---
+++ PATH/src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Nav = () => {
+return (
+    <Nav>
+        <div className="navbar">
+            <ul className="navbar-list">
+                <li>
+                    
+                </li>
+            </ul>
+            
+            
+            
+            </div>
+    </Nav>
+)
+};
+
+export default Nav;
